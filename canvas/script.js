d932802a0d800eda4654b6092662392021aea308
gasperpb <you@example.com>
2021-07-29 11:50:12 -0300

    First commit

---
+++ canvas/script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = 'red';
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 5;
+ctx.beginPath();
+ctx.arc(100, 300, 50, 0, Math.PI * 2);
+ctx.closePath();
+ctx.fill();
+ctx.stroke();
